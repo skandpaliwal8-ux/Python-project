57f3dfee428c4b753c17748193314f651cfa9cc0
skandpaliwal8-ux <skandpaliwal8@gmail.com>
2025-11-25 01:40:24 +0530

    final changes

--- game_logic.py
+++ game_logic.py
@@ -1,7 +1,9 @@
-import random                       #allows you to play against a bot
+import random
+
+
 def edit_board(position,board):
     #king black board black piece white board white peice
-    piece=["| ⛁ ","│ ░ ","│ ○ ", "│ ▓ ","│ ● "]                         #
+    piece=["| ⛁ ","│ ░ ","│ ○ ", "│ ▓ ","│ ● "]
     for i in range(len(board)):
         if i==0:
             continue
@@ -30,72 +32,76 @@ def edit_board(position,board):
                         board[i][j]=piece[4]
                     break
     return board
-
-def possible_moves(position, player, r, c):           #function that displays all the possible moves for a player
-    moves = []
-    captures = []
+#function for getting the possible moves
+def possible_moves(position,player,r,c,l):
+    moves=[]
+    captures=[]
     if player == 0: 
-        directions = [(1, -1), (1, 1)]
+        dir=[(1,-1),(1,1)]
     else:  
-        directions = [(-1, -1), (-1, 1)]
-    king = 0
+        dir=[(-1,-1),(-1,1)]
+    king=0
     t=0
     for p in position[player]:
-        if p[0] == r and p[1] == c:
-            king = p[2]
+        if p[0]==r and p[1]==c:
+            king=p[2]
             t=1
             break
     if t==0:
         return moves,captures
-    if king == 1:
-        directions = [(1, -1), (1, 1), (-1, -1), (-1, 1)]
-    enemy = 1 - player
-    for dr, dc in directions:
-        nr = r + dr
-        nc = c + dc
-        if 1 <= nr <= 8 and 1 <= nc <= 8:
-            occupied = False
-            for p in position[0] + position[1]:
-                if p[0] == nr and p[1] == nc:
-                    occupied = True
+    if king==1:
+        dir=[(1,-1),(1,1),(-1,-1),(-1,1)]
+    enemy=1-player
+    for x1,y1 in dir:
+        x2=r+x1
+        y2=c+y1
+        if 1<=x2<=8 and 1<=y2<=8:
+            occupied=False
+            for p in position[0]+position[1]:
+                if p[0]==x2 and p[1]==y2:
+                    occupied=True
                     break
             if not occupied:
-                moves.append([nr, nc])
-        er = r + dr
-        ec = c + dc
-        jr = r + 2*dr
-        jc = c + 2*dc
-        if 1 <= jr <= 8 and 1 <= jc <= 8:
-            enemy_here = False
+                moves.append([x2,y2])
+        x3=r+x1
+        y3=c+y1
+        x4=r+2*x1
+        y4=c+2*y1
+        if 1<=x4<=8 and 1<=y4<=8:
+            enem=False
             for p in position[enemy]:
-                if p[0] == er and p[1] == ec:
-                    enemy_here = True
+                if p[0]==x3 and p[1]==y3:
+                    enem=True
                     break
-            if enemy_here:
-                landing_empty = True
-                for p in position[0] + position[1]:
-                    if p[0] == jr and p[1] == jc:
-                        landing_empty = False
+            if enem:
+                land=True
+                for p in position[0]+position[1]:
+                    if p[0]==x4 and p[1]==y4:
+                        land=False
                         break
-                if landing_empty:
-                    captures.append([jr, jc])
-    if moves!=[]:
-        print(r,c,"can move to",moves)
+                if land:
+                    captures.append([x4,y4])
+    if moves!=[] and captures==[]:
+        if (l==1 and player!=1) or l==2:
+            print(r,c,"can move to",moves)
     return moves, captures
 
-def forced_move(position, player):                                     #function that displays all the captures for a given player
-    forced = []
+#for checking if forced moves are there
+def forced_move(position, player,l):
+    forced=[]
     pos_moves={}
     for piece in position[player]:
-        r, c, k = piece
-        t=f"{r} {c}"
-        moves, captures = possible_moves(position, player, r, c)
+        x1, x2, k = piece
+        t=f"{x1} {x2}"
+        moves, captures = possible_moves(position, player,x1,x2,l)
         pos_moves[t]=moves
-        if captures:
-            forced.append([r, c])
+        for i in captures:
+            forced.append([[x1,x2],i])
+    #pos_moves contains all moves for each piece
     return forced,pos_moves
 
-def queen(position,move):                        #function that tracks when a piece reaches the end and becomes a king
+#for making king
+def queen(position,move):
     for i in position[move]:
         if move==0:
             if i[0]==8:
@@ -105,35 +111,36 @@ def queen(position,move):                        #function that tracks when a pi
                 i[2]=1
     return position
 
-def update_position(position, old_r, old_c, new_r, new_c, player):           #function that changes the position of the board when a move is made
-    cap = False
+def update_position(position,x1,y1,x2,y2,player):
+    #cap is if piece captured
+    cap=False
     for piece in position[player]:
-        if piece[0] == old_r and piece[1] == old_c:
-            piece[0] = new_r
-            piece[1] = new_c
+        if piece[0]==x1 and piece[1]==y1:
+            piece[0]=x2
+            piece[1]=y2
             break
-    # detect and remove captured piece (capture if moved by 2)
-    if abs(new_r - old_r) == 2 and abs(new_c - old_c) == 2:
-        po1 = old_r + (new_r - old_r)//2
-        po2 = old_c + (new_c - old_c)//2
-        other = 1 - player
+    #detecting piece capture
+    if abs(x2-x1)==2 and abs(y2-y1)==2:
+        po1=x1+(x2-x1)//2
+        po2=y1+(y2-y1)//2
+        other=1-player
         for enemy in position[other]:
-            if enemy[0] == po1 and enemy[1] == po2:
+            if enemy[0]==po1 and enemy[1]==po2:
                 position[other].remove(enemy)
-                cap = True
+                cap=True
                 break
-    return position, cap
-
-def win(position):                 #function that decides when a player has won a game
+    return position,cap
+#check win
+def win(position):
     if position[0]==[]:
         print("Player2 wins")
         return 0
     elif position[1]==[]:
         print("Player1 wins")
         return 0
-
-def input_moves(prompt_text="Enter move: "):  #takes the input of the user
-    move_input=input(prompt_text).split()
+#inputting moves from user
+def input_moves(a="Enter move: "):
+    move_input=input(a).split()
     cords=[]
     for i in move_input:
         if i.isdigit()==True:
@@ -145,7 +152,7 @@ def input_moves(prompt_text="Enter move: "):  #takes the input of the user
         else:
                 print("Enter correct input")
                 return 0
-    if len(cords) < 2:
+    if len(cords)<2:
         print("Enter correct input")
         return 0
     if cords[0]>8 or cords[1]>8 or cords[0]<1 or cords[1]<1:
@@ -153,25 +160,21 @@ def input_moves(prompt_text="Enter move: "):  #takes the input of the user
         return 0
     return cords
 
-
+#changes between player1 and 2
 def inverse(player):
     if player==0:
         return 1
     else:
         return 0
-
-def bot_select(forced,pos_moves):            #function that allows you to play against the bot
-    if forced:
-        key = f"{forced[0][0]} {forced[0][1]}" #please note that the bot does not use any thinking and does random  moves
-        moves = pos_moves[key]
-        chosen = random.choice(moves)
-        return [forced[0][0], forced[0][1]], chosen
+#bot selecting movevs function
+def bot_select(forced,pos_moves):
+    if forced!=[]:
+        s=random.choice(forced)
+        return s[0],s[1]
     items = [(k,v) for k,v in pos_moves.items() if v]
     if not items:
         return None
-    key, moves = random.choice(items)
-    r, c = map(int, key.split())
-    chosen = random.choice(moves)
-    return [r, c], chosen
-        
-
+    key,moves=random.choice(items)
+    a,b=map(int, key.split())
+    chosen=random.choice(moves)
+    return [a, b], chosen
--- main.py
+++ main.py
@@ -6,89 +6,90 @@ position = position()
 from game_logic import edit_board, possible_moves, forced_move, update_position, queen, win, input_moves, inverse,bot_select
 
 #player1 is black player2 is white
-def game(board, position):
+def main(board, position):
     print("Welcome to checkers!!!!")
-    q = input("Load previous game? (Y/N): ").lower()              #basic interface when running the code that allows you to load a saved game
-    player = 0
+    q=input("Load previous game? (Y/N): ").lower()              #basic interface when running the code that allows you to load a saved game
+    player=0
     t=0
-    if q == "y":
-        lp, pl = load_game()
+    if q=="y":
+        lp,pl=load_game()
         if lp is not None:
-            position = lp
-            player = pl
+            position=lp
+            player=pl
     try:
-        l = int(input("1. Bot\n2. 1v1\nChoose: "))
+        l=int(input("1. Bot\n2. 1v1\nChoose: "))
     except:
-        l = 2
+        l=2
 
     while True:
-        board = edit_board(position, board)
+        board=edit_board(position, board)
         print_board(board)
         print(f"Player{player+1}'s turn")
-        forced, pos_moves = forced_move(position, player)
-        if all(len(x) == 0 for x in pos_moves.values()):                                     #when the game ends
+        forced,pos_moves=forced_move(position,player,l)
+        c=[]
+        for i in forced:
+            c.append(i[0])
+        if all(len(x)==0 for x in pos_moves.values()):                                     #when the game ends
             print(f"Player{inverse(player)+1} wins!!")
             break
 
-        if player == 1 and l == 1:
-            start, end = bot_select(forced,pos_moves)
+        if player==1 and l==1:
+            start,end=bot_select(forced,pos_moves)
             if start is None:
                 break
-            moves, captures = possible_moves(position, player, start[0], start[1])
-            if end==[]:
-                end=random.choice(captures)
+            moves,captures=possible_moves(position,player,start[0],start[1],l)
         else:
-            cords = input_moves()
-            if cords == 1:
+            cords=input_moves()
+            if cords==1:
                 save_game(position,player)
                 break
             if not cords:
                 continue
             start = cords
-            moves, captures = possible_moves(position, player, start[0], start[1])
-            if forced and start not in forced:
+            moves,captures=possible_moves(position,player,start[0],start[1],l)
+            if c and start not in c:
                 print("Must capture.")
                 continue
             print("Moves:", moves)
             print("Captures:", captures)
-            cur = input_moves()
-            if cur == 1:
+            cur=input_moves()
+            if cur==1:
                 save_game(position, player)
                 continue
-            end = cur
-            if forced and end not in captures:
+            end=cur
+            if c and end not in captures:
                 print("Must capture.")
                 continue
-            if not forced and end not in moves:
+            if not c and end not in moves:
                 print("Invalid.")
                 continue
 
-        position, cap = update_position(position, start[0], start[1], end[0], end[1], player)
-        position = queen(position, player)
+        position,cap=update_position(position,start[0],start[1],end[0],end[1],player)
+        position=queen(position, player)
 
         if cap:
-            rr, cc = end
+            rr,cc=end
             while True:
-                cmove, ccaps = possible_moves(position, player, rr, cc)
+                cmove,ccaps=possible_moves(position,player,rr,cc,l)
                 if not ccaps:
                     break
-                if player == 1 and l == 1:
-                    nxt = random.choice(ccaps)
+                if player==1 and l==1:
+                    nxt=random.choice(ccaps)
                 else:
                     print("Next captures:", ccaps)
-                    nxt = input_moves("Enter next capture: ")
-                if nxt == 1:
+                    nxt=input_moves("Enter next capture: ")
+                if nxt==1:
                     save_game(position, player)
                     continue                                                                   ##main function to run the game
                 if nxt not in ccaps:
                     continue
-                position, _ = update_position(position, rr, cc, nxt[0], nxt[1], player)
-                position = queen(position, player)
-                rr, cc = nxt[0], nxt[1]
+                position,p=update_position(position,rr,cc,nxt[0],nxt[1],player)
+                position=queen(position,player)
+                rr,cc=nxt[0],nxt[1]
 
-        k = win(position)
-        if k == 0:
+        k=win(position)
+        if k==0:
             break
-        player = inverse(player)
+        player=inverse(player)
 
-game(board, position)
+main(board,position)
