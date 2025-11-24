39d3c8a37e5dc5da8307a2601114035fd664e3df
sidcodez24 <sidkennedy24@gmail.com>
2025-11-24 22:24:58 +0530

    Update board.py

--- board.py
+++ board.py
@@ -1,6 +1,6 @@
 def board():
     board = [
-    ["    ", "  1 ", "  2 ", "  3 ", "  4 ", "  5 ", "  6 ", "  7 ", "  8 "],
+    ["    ", "  1 ", "  2 ", "  3 ", "  4 ", "  5 ", "  6 ", "  7 ", "  8 "], # Layout of the board using (number,number) coordinate system
     ["  1 ", "│ ░ ", "│ ● ", "│ ░ ", "│ ● ", "│ ░ ", "│ ● ", "│ ░ ", "│ ● "],
     ["  2 ", "│ ● ", "│ ░ ", "│ ● ", "│ ░ ", "│ ● ", "│ ░ ", "│ ● ", "│ ░ "],
     ["  3 ", "│ ░ ", "│ ● ", "│ ░ ", "│ ● ", "│ ░ ", "│ ● ", "│ ░ ", "│ ● "],
@@ -14,10 +14,10 @@ def board():
 #starting position
 def position():
     position = [
-    [   # blaskce pieces
-        [1,2,0], [1,4,0], [1,6,0], [1,8,0],
-        [2,1,0], [2,3,0], [2,5,0], [2,7,0],
-        [3,2,0], [3,4,0], [3,6,0], [3,8,0]
+    [   # black pieces
+        [1,2,0], [1,4,0], [1,6,0], [1,8,0],                      ##stores the coordinates of all the pieces on the board on the start,
+        [2,1,0], [2,3,0], [2,5,0], [2,7,0],                      ## the first two numbers signify the coordinates of the piece
+        [3,2,0], [3,4,0], [3,6,0], [3,8,0]                       ## the last number signifies whether the piece is a king or not 0 for not 1 if yes
     ],
     [   # whitew pieces
         [6,1,0], [6,3,0], [6,5,0], [6,7,0],
